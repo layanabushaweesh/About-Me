9f4dd6a8db14cd292b9097be43bf82977cfdee4a
layanabushaweesh <layanabushaweesh@gmail.com>
2021-02-08 16:43:33 +0200

    change

---
+++ main.js
@@ -0,0 +1,70 @@
+'use strict';
+let userName = prompt(' enter your name please');
+alert(  " hi " + userName);
+alert ( ' you are welcome');
+console.log ( userName);
+
+let color = prompt ( ' my favorite color is pink ?');
+if(color.toLowerCase() === 'yes' || color.toLowerCase()  ===  'y'  ){
+  alert('that it\'s right');
+ // console.log( 'that it\'s right'  );
+}
+   else if ( color.toLowerCase() === 'no'   ||   color.toLowerCase() === 'n' ) {
+     alert ('that it\'s wrong');
+     //console.log ( ' that it\'s wrong'  );
+   }
+   else {
+     alert ( ' please answer yes or no');
+   }
+
+   let age = prompt ( ' my age is 25?');
+
+if(age.toLowerCase() === 'yes'    ||  color.toLowerCase()=== 'y' )  {
+  alert('that it\'s right');
+  //console.log( 'that it\'s right'  );
+}
+    else if ( age.toLowerCase() === 'no'   ||  color.toLowerCase() === 'n'  ) {
+     alert ( ' that it\'s wrong');
+     //console.log ( ' that it\'s wrong'  );
+   }
+   else {
+     alert ( ' please answer yes or no');
+   }
+
+   let name = prompt ( ' my name is layan ?');
+if(name.toLowerCase() === 'yes'   || color.toLowerCase()=== 'y'){
+  alert('that it\'s right');
+  //console.log( 'that it\'s right'  );
+}
+   else if ( name.toLowerCase() === 'no'     ||  color.toLowerCase() === 'n'   ) {
+     alert ( ' that it\'s wrong');
+     //console.log ( ' that it\'s wrong'  );
+   }
+   else {
+     alert ( ' please answer yes or no');
+   }
+   let dadName= prompt ( ' my dad name is jamil ?');
+   if(dadName.toLowerCase() === 'yes'  ||   color.toLowerCase()=== 'y' ){
+     alert('that it\'s right');
+     //console.log( 'that it\'s right'  );
+   }
+      else if ( dadName.toLowerCase() === 'no'  ||   color.toLowerCase() === 'n' ) {
+        alert ( ' that it\'s wrong');
+       // console.log ( ' that it\'s wrong'  );
+      }
+      else {
+        alert ( ' please answer yes or no');
+      }
+   
+      let mumName= prompt ( ' my mum name is lina ?');
+      if(mumName.toLowerCase() === 'yes' || color.toLowerCase()==='y'){
+        alert('that it\'s right');
+        //console.log( 'that it\'s right'  );
+      }
+         else if ( mumName.toLowerCase() === 'no' ||  color.toLowerCase() === 'n'  ) {
+           alert ( ' that it\'s wrong');
+           //console.log ( ' that it\'s wrong'  );
+         }
+         else {
+           alert ( ' please answer yes or no');
+         }
